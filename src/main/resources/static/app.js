7988258758a333d2e37d47d622f805ad4ced2bf8
alexamazzing <alexamazzing@gmail.com>
2022-05-02 16:51:44 +0500

    *

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,17 +1,19 @@
 const url = 'http://localhost:8080/api/users'
 const urlRoles = 'http://localhost:8080/api/roles'
-const usersList = document.querySelector('.usersList')
+let usersList = document.querySelector('.usersList')
 let result = ''
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        drawTable(data)
-        console.log(data)
-    })
-    .catch(err => console.log(err))
-
+getUsers()
+function getUsers() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            drawTable(data)
+        })
+        .catch(err => console.log(err))
+}
 const drawTable = (users) => {
+    usersList.innerHTML = ''
+    result = ''
     users.forEach( user => {
         let userRoles = ``
         for(let role of user.roles) {
@@ -40,28 +42,27 @@ const on = (element, event, selector, handler) => {
     })
 }
 
-
 const editModal = new bootstrap.Modal(document.getElementById('editModal'))
 const deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'))
 
 on(document, 'click', '.editButton', e => {
     const tableRow = e.target.parentNode.parentNode
-    editIdForm = tableRow.children[0].innerHTML
+    let editIdForm = tableRow.children[0].innerHTML
 
-    fetch(url +'/' + editIdForm)
+    fetch(url + '/' + editIdForm)
         .then(res => res.json())
         .then(data => showEditModal(data))
 })
 
 on(document, 'click', '.deleteButton', e => {
     const tableRow = e.target.parentNode.parentNode
-    deleteIdForm = tableRow.children[0].innerHTML
+    let deleteIdForm = tableRow.children[0].innerHTML
 
-    fetch(url +'/' + deleteIdForm)
+    fetch(url + '/' + deleteIdForm)
         .then(res => res.json())
         .then(data => showDeleteModal(data))
 })
-
+//=================== edit ===========================
 const showEditModal = (user) => {
     const editId = document.getElementById('edit_id_input')
     const editName = document.getElementById('edit_name_input')
@@ -93,8 +94,49 @@ const showEditModal = (user) => {
         });
 
     editModal.show()
-}
 
+    const editUserButton = document.querySelector('.editUserButton')
+    editUserButton.addEventListener('click', e => {
+        e.preventDefault()
+
+        let rolesFromForm = document.getElementById('edit_roles_select')
+        let selectedOptions = rolesFromForm.selectedOptions
+        let selected = []
+        for (let i = 0; i < selectedOptions.length; i++) {
+            selected.push({id: selectedOptions[i].value, name: 'ROLE_' + selectedOptions[i].text})
+        }
+        let prevPass = user.password
+        let pass;
+        if (document.getElementById('edit_password_input').value === '') {
+            pass = prevPass
+        } else {
+            pass = document.getElementById('edit_password_input').value
+        }
+        let data = {
+            id: editId.value,
+            name: editName.value,
+            surname: editSurname.value,
+            age: editAge.value,
+            email: editEmail.value,
+            password: pass,
+            roles: selected
+        }
+        console.log(data)
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(getUsers)
+
+        editModal.hide()
+        document.getElementById('edit_password_input').value = ''
+    })
+}
+//=================== edit ===========================
+//=================== delete =========================
 const showDeleteModal = (user) => {
     const deleteId = document.getElementById('delete_id_input')
     const deleteName = document.getElementById('delete_name_input')
@@ -126,8 +168,19 @@ const showDeleteModal = (user) => {
         });
 
     deleteModal.show()
-}
 
+    const deleteUserButton = document.querySelector('.deleteUserButton')
+    deleteUserButton.addEventListener('click', e => {
+        e.preventDefault()
+        fetch(url + '/' + user.id, {
+            method: 'DELETE',
+        })
+            .then(getUsers)
+        deleteModal.hide()
+    })
+}
+//=================== delete =========================
+//=================== add ============================
 const name = document.getElementById('name_input')
 const surname = document.getElementById('surname_input')
 const age = document.getElementById('age_input')
@@ -142,56 +195,33 @@ fetch(urlRoles)
         })
     })
 
-
-
 const addUserButton = document.querySelector('#addUser')
-
 addUserButton.addEventListener('click',
     e => {
         e.preventDefault()
-
         let rolesFromForm = document.getElementById('roles')
         let selectedOptions = rolesFromForm.selectedOptions
         let selected = []
-        for (let i=0; i < selectedOptions.length; i++) {
-            selected.push(selectedOptions[i].value)
+        for (let i = 0; i < selectedOptions.length; i++) {
+            selected.push({id: selectedOptions[i].value, name: 'ROLE_' + selectedOptions[i].text})
         }
-        let userRoles = new Array()
 
-        fetch(urlRoles)
-            .then(res => res.json())
-            .then(roles => {
-            roles.forEach(role => {
-                selected.forEach(roleId => {
-                    if (role.id == roleId) {
-                        userRoles.push(role);
-                    }
-                });
-            });
-        });
-        console.log(userRoles)
         let data = JSON.stringify({
             name: name.value,
             surname: surname.value,
             age: age.value,
             email: email.value,
             password: password.value,
-            roles: userRoles
+            roles: selected
         })
-        console.log(data)
-        // fetch(url, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: data
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         const newUser = []
-        //         newUser.push(data)
-        //         drawTable(newUser)
-        //     })
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+            .then(getUsers)
         name.value = ''
         surname.value = ''
         age.value = ''
@@ -199,46 +229,4 @@ addUserButton.addEventListener('click',
         password.value = ''
         formRoles.value = ''
     })
-
-/*const deleteUserButton = document.querySelector('')
-addUserButton.addEventListener('click', e => {
-    e.preventDefault()
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            //TODO
-        })
-    })
-        .then(res => res.json())
-        .then(data => {
-            const newUser= []
-            newUser.push(data)
-            drawTable(newUser)
-        })
-
-})
-const editUserButton = document.querySelector('')
-addUserButton.addEventListener('click', e => {
-    e.preventDefault()
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            //TODO
-        })
-    })
-        .then(res => res.json())
-        .then(data => {
-            const newUser= []
-            newUser.push(data)
-            drawTable(newUser)
-        })
-
-})*/
-
-
+//=================== add ============================
